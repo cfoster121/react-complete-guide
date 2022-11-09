e3a4291d161859d4eb3329185a010939b26803a6
cfoster121 <cfoster649@gmail.com>
2022-11-09 12:53:12 -0800

    Added components

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+import './ExpenseItem.css'
+
+const ExpenseItem = () => {
+    return (
+        <div>
+            <h1>Expense item component</h1>
+            <p>Date</p>
+            <p>Amount</p>
+        </div>
+    )
+}
+
+export default ExpenseItem
