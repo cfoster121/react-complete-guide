f19179b466a1f396e77d6441b54d29e10b288379
Courtney Foster <cmf@Courtneys-MBP.attlocal.net>
2022-11-17 14:32:28 -0800

    Set up children attributes

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,17 @@
-import ExpenseDate from './ExpenseDate'
-import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
+import './ExpenseItem.css';
 
-const ExpenseItem = (props) => {
-
-return (
-    <div className='expense-item'>
-        <ExpenseDate date={props.date} />
-    <div className= 'expense-item__description'>
+function ExpenseItem(props) {
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
         <h2>{props.title}</h2>
-    </div>
-    </div>
-)
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+    </Card>
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
